b94da36f4d345ee23f34eb13d32fd8ea9d1a2057
Sergio <ser.humano@gmail.com>
2014-08-29 01:30:00 -0500

    agregados archivos iniciales

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta content='width=device-width, initial-scale=1' name='viewport'>
+	<meta name="Title" content="title">
+	<meta name="Description" content="Description">
+	<meta name="Keywords" content="keyword, keywords">
+	<title>Document</title>
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<link rel="stylesheet" media="screen and (min-width: 600px)" href="css/style.less" type="text/less" />
+	<!--[if lt IE 9]><script type="text/javascript" src="js/html5shiv"></script><![endif]-->
+		
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800,600' rel='stylesheet' type='text/css'>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<script src="js/modernizr.js"></script>
+
+    <!-- Scripts -->
+	<script src="js/less.js" type="text/javascript"></script>
+	<script>sless.watch();</script>
+</head>
+<body>
+holi <br>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos doloremque, vero maiores ab minus similique a optio! Ipsam quod fugiat ducimus ipsum incidunt, vel sit, nobis minus! Totam, modi, atque. <br>
+<a href="#">test de enlace</a><br>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere consequatur possimus quia, laboriosam, voluptatum dolorum perferendis assumenda repellat accusantium magni consequuntur fugiat quibusdam dicta, soluta harum expedita non a ab.
+</body>
+</html>
